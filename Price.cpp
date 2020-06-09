4d1fee3f9a98e492af311a6e9beaf605f23f6bf5
y-c-mby <211525852@mby.co.il>
2020-06-09 14:02:33 +0300

    first commit

---
+++ Price.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mby on 09/06/2020.
+//
+
+#include "Price.h"
---
+++ Price.h
@@ -0,0 +1,18 @@
+//
+// Created by mby on 09/06/2020.
+//
+
+#ifndef PRICE_PRICE_H
+#define PRICE_PRICE_H
+
+
+class Price {
+public:
+    Price();
+private:
+    int price;
+
+};
+
+
+#endif //PRICE_PRICE_H
